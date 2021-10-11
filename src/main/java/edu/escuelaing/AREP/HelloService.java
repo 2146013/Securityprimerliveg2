757ef6aa2e0a5449496bb1bc8eec3b24e3c24ec5
Daniela pachon <laura.pachon-c@mail.escuelaing.edu.co>
2021-10-10 20:14:48 -0500

    laboratorio

---
+++ src/main/java/edu/escuelaing/AREP/HelloService.java
@@ -0,0 +1,18 @@
+package edu.escuelaing.AREP;
+
+import static spark.Spark.*;
+
+public class HelloService {
+    public static void main(String... args){
+        secure("keystores/ecikeystore.p12","123456",null,null);
+        port(getPort());
+        get("hello",(req,res) -> "hello world");
+
+    }
+    static int getPort() {
+        if (System.getenv("PORT") != null) {
+            return Integer.parseInt(System.getenv("PORT"));
+        }
+        return 4567; //returns default port if heroku-port isn't set (i.e. on localhost)
+    }
+}
